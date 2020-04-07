ebcfae9ba9ef855947c5e3075e2c21127e740649
Tuan <tuandhth1907031@fpt.edu.vn>
2020-04-07 13:08:57 +0700

    Doi ten

--- src/main/java/demo/MainThread.java
+++ src/main/java/demo/MainThread.java
@@ -2,6 +2,6 @@ package demo;
 
 public class MainThread {
     public static void main(String[] args) {
-        System.out.println("Hello Tuan");
+        System.out.println("Hello Hai Tuan");
     }
 }
